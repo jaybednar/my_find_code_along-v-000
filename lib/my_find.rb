514f3ee87793dab142cbd83d345f06be6cb71c55
Jay Bednar <ironviralmedia@gmail.com>
2018-02-07 01:49:59 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,10 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    return collection[i] if yield(collection[i]) 
+    return collection[i] if yield(collection[i])
     i += 1
   end
-
-  if return_collection.include?(true)
-    return_collection[]
 end
